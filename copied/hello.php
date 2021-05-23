2d2a21b964e2b77c0c9c29e8d842335c748cd869
Author <actions@github.com>
2021-05-23 03:12:24 +0000

    Automated Change
    Signed-off-by: My GitHub Actions Bot <my-github-actions-bot@example.org>

---
+++ copied/hello.php
@@ -0,0 +1,8 @@
+<?php
+echo 'hello world!';
+echo 'isnt it the action?';
+echo 'oh i accidentally deleted actions yml by force-pushing';
+echo 'maybe the mentainer replaced all the "test" to "target" and "latest" became "latarget" by their mistake';
+echo 'how about these token settings ya!!?';
+echo 'i failed cuz of typo by myself....';
+echo 'how about auto-commiting version?';
